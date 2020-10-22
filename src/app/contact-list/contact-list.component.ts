480d136ccae6ea6ab3e54e4cadc3d3b09f812ea0
harshadvict <harshadvict@gmail.com>
2020-10-22 13:07:37 +0530

    completed

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,6 +1,7 @@
 import { contacts } from './../contact';
 import { userContact } from './../userContact';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -15,13 +16,24 @@ export class ContactListComponent implements OnInit {
 
   ngOnInit() {
   // initialize your contacts here
+  this.contacts=contacts;
   }
 
   addContact(newContact): any {
     console.log('Add contact has been called');
      // Fill your code here
     // add contact to contacts list
-    // clear inputs
-   
+    console.log(newContact);
+    const newObj: userContact =Object.assign({},this.newContact);
+    this.newContact.name=newContact.name;
+    this.newContact.email=newContact.email;
+    this.newContact.phoneNumber=newContact.phoneNumber;
+    this.newContact.image=newContact.image;
+    contacts.push(newObj);
+    console.log(contacts);
+    
+  }
+  formReset(formval : NgForm){
+    formval.reset();
   }
 }
